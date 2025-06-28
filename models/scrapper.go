c145bd0db0b3ccc775bc7f90ae408839fd3ff2de
Milind Gour <milind.gour@nagarro.com>
2025-06-29 01:35:15 +0530

    Fix minor typo

--- models/scrapper.go
+++ models/scrapper.go
@@ -4,14 +4,14 @@ type MovieResult struct {
 	Name          string `json:"name"`
 	Description   string `json:"description"`
 	YearOfRelease int    `json:"yearOfRelease"`
-	ThumnailUrl   string `json:"thumnailUrl"`
+	ThumnailUrl   string `json:"thumbnailUrl"`
 }
 
 type TVResult struct {
 	Name          string       `json:"name"`
 	Description   string       `json:"description"`
 	YearOfRelease int          `json:"yearOfRelease"`
-	ThumnailUrl   string       `json:"thumnailUrl"`
+	ThumnailUrl   string       `json:"thumbnailUrl"`
 	TotalSeasons  int          `json:"totalSeasons"`
 	Seasons       []SeasonInfo `json:"seasons"`
 }
