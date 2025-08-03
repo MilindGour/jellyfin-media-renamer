91d76627ca1d5518aeeb0c64736dc3a2de019b17
Milind Gour <milind.gour@nagarro.com>
2025-08-03 23:44:39 +0530

    Allow all domains

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,26 +1,24 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
-
-	"github.com/MilindGour/jellyfin-media-renamer/util"
 )
 
 func CorsMW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Receiving request on path:", r.URL)
 
-		if !util.IsProduction() {
-			// Set CORS headers for every request
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Credentials", "true") // if needed
+		// Set CORS headers for every request
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true") // if needed
 
-			// Handle preflight
-			if r.Method == http.MethodOptions {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
+		// Handle preflight
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
 		}
 
 		next.ServeHTTP(w, r)
