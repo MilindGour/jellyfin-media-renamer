857266e1acc81a26da994a830771377459282222
Milind Gour <milind.gour@nagarro.com>
2025-10-23 15:55:48 +0530

    Improvements 03.02

--- frontend/src/lib/components/mediasearchlist/media-searchlist-item.svelte
+++ frontend/src/lib/components/mediasearchlist/media-searchlist-item.svelte
@@ -12,11 +12,14 @@
 <label class="media-searchlist-item flex items-center gap-2 rounded bg-gray-100 p-2">
 	<input class="" type="radio" value={item.mediaId} {name} {onchange} />
 	<section class="info flex gap-2">
-		<img
-			class="aspect-[2/3] max-h-16 rounded"
-			src={item.thumbnailUrl || 'noimg.svg'}
-			alt="poster"
-		/>
+		<div class="img-wrapper min-w-[43px]">
+			<img
+				class="aspect-[2/3] max-h-16 rounded"
+				height="64"
+				src={item.thumbnailUrl || 'noimg.svg'}
+				alt="poster"
+			/>
+		</div>
 		<section class="text">
 			<h3 class="text-md font-medium">{item.name} ({item.yearOfRelease})</h3>
 			<p class="line-clamp-2 text-sm text-gray-500">{item.description || 'No Description'}</p>
