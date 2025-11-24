ea0d368a6bf3a861f94710cb37436c9003b10862
Milind Gour <gour.milind@gmail.com>
2025-11-24 19:17:36 +0530

    fix sveltekit hook for POST requests

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,18 +3,23 @@ import { env } from '$env/dynamic/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
   if (event.url.pathname.startsWith('/backend')) {
-    const { pathname, search } = event.url;
-    const { method, headers, json } = event.request;
-    const contentLength = +(headers.get('Content-Length') || 0);
-    const hasBody = contentLength > 0;
-    const body = hasBody && await json() || undefined;
+    const backendURL = `${env.API_BASE}${event.url.pathname.replace('/backend', '')}${event.url.search}`;
+    if (['GET', 'HEAD'].includes(event.request.method)) {
+      const res = await fetch(backendURL, {
+        method: "GET",
+        headers: event.request.headers
+      });
+      return new Response(await res.text(), { headers: { 'Content-Type': 'application/json' } });
+    }
 
-    const backendURL = `${env.API_BASE}${pathname.replace('/backend', '')}${search}`;
+    const contentLength = +(event.request.headers.get('Content-Length') || 0);
+    const hasBody = contentLength > 0;
+    const b = hasBody ? await event.request.text() : undefined;
 
     const backendResponse = await fetch(backendURL, {
-      body,
-      headers,
-      method
+      body: b,
+      headers: event.request.headers,
+      method: event.request.method
     });
 
     return new Response(await backendResponse.text(), { headers: { 'Content-Type': 'application/json' } });
